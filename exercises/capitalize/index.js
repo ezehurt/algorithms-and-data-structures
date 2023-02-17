622c7ceab5344f2f8cb13ac651c7d4f9ee4406ed
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2023-02-17 17:21:47 +0100

    added capitalize excercise

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,35 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
-
+function capitalize(str) {
+    return str.split("").map((char, index, arr) => {
+        if (arr[index - 1] === ' ' || index === 0) {
+            return char.toUpperCase();
+        } else {
+            return char;
+        }
+    }).join("");
+}
+capitalize('a short sentence')
 module.exports = capitalize;
+
+
+// EJHF Solution 1
+// function capitalize(str) {
+//     return str.split(" ").map(word => {
+//         const lastChars = word.split("").slice(1);
+//         const capitalizedFirstChar = word[0].toUpperCase()
+//         return [capitalizedFirstChar,...lastChars].join("")
+//     }).join(" ");
+// }
+
+// EJHF Solution 2
+// function capitalize(str) {
+//     return str.split("").map((char, index, arr) => {
+//         if (arr[index - 1] === ' ' || index === 0) {
+//             return char.toUpperCase();
+//         } else {
+//             return char;
+//         }
+//     }).join("");
+// }
