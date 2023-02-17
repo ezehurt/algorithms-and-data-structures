ed96e39e5132f5633fd4edb0e5d0cbabe9564d5d
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2023-02-17 09:50:25 +0100

    added reverseint excercise

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    return Number(Math.abs(n).toString().split('').reverse().join('')) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// EJHF Solution
+// function reverseInt(n) {
+//     const reversedAbsNumber = Number(Math.abs(n).toString().split('').reverse().join(''));
+//     return n < 0 ? reversedAbsNumber * - 1 : reversedAbsNumber;
+// }
