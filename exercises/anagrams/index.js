bf6953ef5c5ed33bd1a9e947ef919a3f4542986b
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2023-02-17 16:45:07 +0100

    added anagrams excercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,92 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function stringToObject(str) {
+    const stringToMap = {};
+    const arr = str.split('')
+    for (let i = 0; i < arr.length; i++) {
+        if (!stringToMap[arr[i]]) {
+            stringToMap[arr[i]] = 1;
+        } else {
+            stringToMap[arr[i]] = stringToMap[arr[i]] + 1;
+        }
+    }
+    return stringToMap;
+}
 
+function compareObject(objectA, ObjectB) {
+    const keys1 = Object.keys(objectA);
+    const keys2 = Object.keys(ObjectB);
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+    for (let key of keys1) {
+        if (objectA[key] !== ObjectB[key]) {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+function removeAllSpecialCharactersAndWhiteSpaces(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase();
+}
+
+function anagrams(stringA, stringB) {
+    return removeAllSpecialCharactersAndWhiteSpaces(stringA).split('').sort().join('') === removeAllSpecialCharactersAndWhiteSpaces(stringB).split('').sort().join('') 
+}
+anagrams('rail safety', 'fairy tales')
 module.exports = anagrams;
+
+// EJHF Solution 1
+// function stringToObject(str) {
+//     const stringToMap = {};
+//     const arr = str.split('')
+//     for (let i = 0; i < arr.length; i++) {
+//         if (!stringToMap[arr[i]]) {
+//             stringToMap[arr[i]] = 1;
+//         } else {
+//             stringToMap[arr[i]] = stringToMap[arr[i]] + 1;
+//         }
+//     }
+//     return stringToMap;
+// }
+
+// function removeAllSpecialCharactersAndWhiteSpaces(str) {
+//     return str.replace(/[^\w]/g, "").toLowerCase();
+// }
+
+// function compareObject(objectA, ObjectB) {
+//     const keys1 = Object.keys(objectA);
+//     const keys2 = Object.keys(ObjectB);
+//     if (keys1.length !== keys2.length) {
+//         return false;
+//     }
+//     for (let key of keys1) {
+//         if (objectA[key] !== ObjectB[key]) {
+//             return false;
+//         }
+//     }
+//     return true;
+
+// }
+// function anagrams(stringA, stringB) {
+//     const stringAFormatted = removeAllSpecialCharactersAndWhiteSpaces(stringA);
+//     const stringBFormatted = removeAllSpecialCharactersAndWhiteSpaces(stringB);
+
+//     const stringAMap = stringToObject(stringAFormatted);
+//     const stringBMap = stringToObject(stringBFormatted);
+
+//     return compareObject(stringAMap, stringBMap);
+    
+// }
+
+//EJHF Solution 2
+// function removeAllSpecialCharactersAndWhiteSpaces(str) {
+//     return str.replace(/[^\w]/g, "").toLowerCase();
+// }
+
+// function anagrams(stringA, stringB) {
+//     return removeAllSpecialCharactersAndWhiteSpaces(stringA).split('').sort().join('') === removeAllSpecialCharactersAndWhiteSpaces(stringB).split('').sort().join('') 
+// }
