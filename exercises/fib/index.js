e09b3fab64a0e9c62f1408d773345fe0321812e1
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2023-02-21 13:58:18 +0100

    added fib excercise

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,47 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//     if(n === 0 || n === 1){
+//         return n;
+//     }
+//     let prev = 0;
+//     let current = 1;
+//     let sum = 0;
+//     for(let i = 1; i < n; i ++) {
+//         sum = current + prev;
+//         prev = current;
+//         current = sum;
 
+//     }
+//     return current;
+// }
+
+const memoize = (fn) =>{
+    const cache = {};
+    return function (...args) {
+        if(cache[args]) {
+            return cache[args];
+        } else {
+            result = fn.apply(this,args);
+            cache[args] = result;
+            return result
+        }
+    }
+}
+
+function fib(n) {
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+fib = memoize(fib);
+const ret = fib(4);
+const arr = [];
+for(let i = 0 ; i < 10; i++){
+    arr.push(fib(i));
+}
+console.log("arr",arr)
 module.exports = fib;
+
