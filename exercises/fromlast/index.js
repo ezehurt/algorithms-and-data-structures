db992b2f5ee89d06fbdd30720be2d31a7ca96952
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2023-02-22 17:10:06 +0100

    added fromlast linked list excercise

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,18 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    let slow = list.getAt(0);
+    let fast = list.getAt(0);
+    while(n > 0){
+        fast = fast.next;
+        n--;
+    }
+    while(fast.next){
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return slow;
+}
 
 module.exports = fromLast;
