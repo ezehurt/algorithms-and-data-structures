a83fb9b7e890664ed116fa202a1d4734c01ef292
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2023-02-22 22:40:27 +0100

    added levelwidth excercise

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, 'l'];
+    const levelArr = [0];;
+    while (arr.length > 1) {
+        const node = arr.shift();
+        if(node === 'l'){
+            arr.push('l');
+            levelArr.push(0);
+        } else {
+            arr.push(...node.children)
+            levelArr[levelArr.length -1]++;
+        }
+    }
+    return levelArr;
+}
 
 module.exports = levelWidth;
